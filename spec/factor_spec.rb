20f8707a06cdce455223f80669f9105278f003af
Rupert Adams <RupertNCAdams@aol.com>
2019-02-17 11:46:34 +0000

    changed var to instance var

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -3,10 +3,10 @@ require 'factor'
 describe '#Factor' do
 
   before(:each) do
-    factor = Factor.new
+    @factor = Factor.new
   end
 
   it 'prints a given number as a string' do
-    expect(factor.numb).to eq('4')
+    expect(@factor.numb).to eq('4')
   end
 end
