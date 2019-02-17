fa5cd5fa1d1d00ed8a7f9dec23a504aa38e531b5
Rupert Adams <RupertNCAdams@aol.com>
2019-02-17 11:51:10 +0000

    added test to turn array into string of numbers

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -9,4 +9,8 @@ describe '#Factor' do
   it 'prints a given number as a string' do
     expect(@factor.numb(4)).to eq('4')
   end
+
+  it 'prints an array of numbers as a string' do
+    expect(@factor.arr([2,3,4])).to eq('2,3,4')
+  end
 end
