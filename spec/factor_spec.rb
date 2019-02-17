4fd4ac33f0be544592e2c361000a6642e9796238
Rupert Adams <RupertNCAdams@aol.com>
2019-02-17 11:45:32 +0000

    wrote first test to print a number as string

---
+++ spec/factor_spec.rb
@@ -0,0 +1,12 @@
+require 'factor'
+
+describe '#Factor' do
+
+  before(:each) do
+    factor = Factor.new
+  end
+
+  it 'prints a given number as a string' do
+    expect(factor.numb).to eq('4')
+  end
+end
