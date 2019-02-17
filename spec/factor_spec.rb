676ec7d7131cfa6250f6fd1b2a517a0a4cb3a9a2
Rupert Adams <RupertNCAdams@aol.com>
2019-02-17 11:58:57 +0000

    added factor finder to print factors of given num

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -13,4 +13,8 @@ describe '#Factor' do
   it 'prints an array of numbers as a string' do
     expect(@factor.arr([2,3,4])).to eq('2,3,4')
   end
+
+  it 'finds the factors of a given number' do
+    expect(@factor.factor_find(34)).to eq([1, 2, 17, 34])
+  end
 end
