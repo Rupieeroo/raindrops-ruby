0005762b251a4ba858b32bcd834be7a7de16e4fd
Rupert Adams <RupertNCAdams@aol.com>
2019-02-17 12:01:56 +0000

    added additional factor_find test for clarity

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -17,4 +17,8 @@ describe '#Factor' do
   it 'finds the factors of a given number' do
     expect(@factor.factor_find(34)).to eq([1, 2, 17, 34])
   end
+
+  it 'finds the factors of a different number' do
+    expect(@factor.factor_find(28)).to eq([1, 2, 4, 7, 14, 28])
+  end
 end
