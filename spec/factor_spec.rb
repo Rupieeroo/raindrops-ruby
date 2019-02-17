951c7c0d1d2d6e352591c80da9e87d4e780ae30f
Rupert Adams <RupertNCAdams@aol.com>
2019-02-17 12:03:29 +0000

    added test contexts for method, clarity

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -5,20 +5,26 @@ describe '#Factor' do
   before(:each) do
     @factor = Factor.new
   end
-
-  it 'prints a given number as a string' do
-    expect(@factor.numb(4)).to eq('4')
+  
+  context '#numb' do
+    it 'prints a given number as a string' do
+      expect(@factor.numb(4)).to eq('4')
+    end
   end
 
-  it 'prints an array of numbers as a string' do
-    expect(@factor.arr([2,3,4])).to eq('2,3,4')
+  context '#arr' do
+    it 'prints an array of numbers as a string' do
+      expect(@factor.arr([2,3,4])).to eq('2,3,4')
+    end
   end
 
-  it 'finds the factors of a given number' do
-    expect(@factor.factor_find(34)).to eq([1, 2, 17, 34])
-  end
+  context '#factor_find' do
+    it 'finds the factors of a given number' do
+      expect(@factor.factor_find(34)).to eq([1, 2, 17, 34])
+    end
 
-  it 'finds the factors of a different number' do
-    expect(@factor.factor_find(28)).to eq([1, 2, 4, 7, 14, 28])
+    it 'finds the factors of a different number' do
+      expect(@factor.factor_find(28)).to eq([1, 2, 4, 7, 14, 28])
+    end
   end
 end
