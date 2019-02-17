01a4f16b3f4a31c670a9ed472ad21a5455d00a3e
Rupert Adams <RupertNCAdams@aol.com>
2019-02-17 12:07:55 +0000

    changed numb test to raindrops method

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -5,10 +5,10 @@ describe '#Factor' do
   before(:each) do
     @factor = Factor.new
   end
-  
-  context '#numb' do
+
+  context '#raindrops' do
     it 'prints a given number as a string' do
-      expect(@factor.numb(4)).to eq('4')
+      expect(@factor.raindrops(4)).to eq('4')
     end
   end
 
